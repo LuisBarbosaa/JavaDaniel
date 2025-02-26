0e97ddddb6e208ce1f8cd778e0dbca8b7f02fd72
LuisBarbosaa <luisbarbosaa1212@gmail.com>
2025-02-26 14:16:44 -0300

    segundo exercicio

---
+++ ListaEx-1/src/Cliente.java
@@ -0,0 +1,37 @@
+package src;
+
+public class Cliente {
+
+    public int numeroConta, numeroAgencia;
+    public String nome;
+    public float saldo;
+
+    public Cliente(int numeroConta, int numeroAgencia, String nome, float saldo){
+        this.numeroConta = numeroConta;
+        this.numeroAgencia = numeroAgencia;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public void realizarDeposito (float valor){
+        if(valor > 0){
+            saldo += valor;
+            System.out.println("Deposito de " + valor + " realizado. Saldo atual: " + saldo);
+        } else {
+            System.out.println("Valor invalido para deposito");
+        }
+    }
+
+    public void realizarSaque (float valor){
+        if(valor > 0 && saldo >= valor){
+            saldo -= valor;
+            System.out.println("Saque de " + valor + " realidado. Saldo atual: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente ou valor invalido");
+        }
+    }
+
+    public String dadosCliente (){
+        return "Nome: " + nome + " Conta: " + numeroConta + " Agencia: " + numeroAgencia + " Saldo: " + saldo;
+    }
+}
---
+++ ListaEx-1/src/TestaCliente.java
@@ -0,0 +1,15 @@
+package src;
+public class TestaCliente {
+    public static void main(String[] args) {
+
+        Cliente fulano = new Cliente(99999, 7777, "Fulano", 1000.0f);
+        System.out.println(fulano.dadosCliente());
+        fulano.realizarDeposito(500);
+        fulano.realizarSaque(300);
+
+        Cliente beltrano = new Cliente(88888, 5555, "Beltrano", 1500.0f);
+        System.out.println(beltrano.dadosCliente());
+        beltrano.realizarDeposito(1000);
+        beltrano.realizarSaque(500);
+    }
+}
